789f54d73a05622c8bf3a19b3721f8ef8c62ecf0
Ritankar Patra <ritankarpatra15@gmail.com>
2025-11-11 09:55:11 +0530

    update

--- layout/wrapper/wrapper.tsx
+++ layout/wrapper/wrapper.tsx
@@ -2,7 +2,7 @@ import ResponsiveAppBar from "../header/header";
 import RowAndColumnSpacing from "../footer/footer";
 import Footer from "../footer/footer";
 import Header from "../header/header";
-const Wrapper: React.FC<Props> = ({ children }) => {
+const Wrapper: React.FC= ({ children }:any) => {
   return (
     <>
       {/* <ResponsiveAppBar /> */}
